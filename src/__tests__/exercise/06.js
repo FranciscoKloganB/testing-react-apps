7c361db178a2417eaf48304a9105240121696218
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 13:08:55 +0000

    feat: mocking window geolocation api - closes #6.0

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -5,8 +5,6 @@ import * as React from 'react'
 import {render, screen, act} from '@testing-library/react'
 import Location from '../../examples/location'
 
-// 🐨 set window.navigator.geolocation to an object that has a getCurrentPosition mock function
-
 // 💰 I'm going to give you this handy utility function
 // it allows you to create a promise that you can resolve/reject on demand.
 function deferred() {
@@ -26,47 +24,49 @@ function deferred() {
 // // assert on the resolved state
 
 describe('Location', () => {
-  beforeEach(() => {
-    render(<Location/>)
+  // 🐨 create a fakePosition object that has an object called "coords" with latitude and longitude
+  const fakePosition = {
+    coords: {latitude: 33, longitude: 34},
+  }
+
+  // 🐨 set window.navigator.geolocation to an object that has a getCurrentPosition mock function
+  beforeAll(() => {
+    window.navigator.geolocation = {
+      getCurrentPosition: jest.fn(),
+    }
   })
+
   it('displays the users current location', async () => {
-    // 🐨 create a fakePosition object that has an object called "coords" with latitude and longitude
-    // 📜 https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
-    //
     // 🐨 create a deferred promise here
-    //
-    // 🐨 Now we need to mock the geolocation's getCurrentPosition function
-    // To mock something you need to know its API and simulate that in your mock:
-    // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-    //
-    // here's an example of the API:
-    // function success(position) {}
-    // function error(error) {}
-    // navigator.geolocation.getCurrentPosition(success, error)
-    //
-    // 🐨 so call mockImplementation on getCurrentPosition
+    const {promise, resolve} = deferred()
     // 🐨 the first argument of your mock should accept a callback
     // 🐨 you'll call the callback when the deferred promise resolves
     // 💰 promise.then(() => {/* call the callback with the fake position */})
-    //
+    // 🐨 Now we need to mock the geolocation's getCurrentPosition function
+    window.navigator.geolocation.getCurrentPosition.mockImplementation(
+      callback => {
+        promise.then(() => callback(fakePosition))
+      },
+    )
     // 🐨 now that setup is done, render the Location component itself
-    //
+    render(<Location />)
     // 🐨 verify the loading spinner is showing up
-    // 💰 tip: try running screen.debug() to know what the DOM looks like at this point.
-    //
-    // 🐨 resolve the deferred promise
-    // 🐨 wait for the promise to resolve
-    // 💰 right around here, you'll probably notice you get an error log in the
-    // test output. You can ignore that for now and just add this next line:
-    // act(() => {})
-    //
-    // If you'd like, learn about what this means and see if you can figure out
-    // how to make the warning go away (tip, you'll need to use async act)
-    // 📜 https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
-    //
-    // 🐨 verify the loading spinner is no longer in the document
-    //    (💰 use queryByLabelText instead of getByLabelText)
-    // 🐨 verify the latitude and longitude appear correctly
+    expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+    // 🐨 resolve the deferred promise and wait for the promise to resolve
+    // 💰 right around here, you'll notice an error log in the test output.
+    // You can ignore that for now and just add this next line: act(() => {})
+    await act(async () => {
+      resolve()
+      await promise
+    })
+
+    expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+    expect(screen.getByTestId(/latitude/i)).toHaveTextContent(
+      `Latitude: ${fakePosition.coords.latitude}`,
+    )
+    expect(screen.getByTestId(/latitude/i)).toHaveTextContent(
+      `Longitude: ${fakePosition.coords.longitude}`,
+    )
   })
 })
 
