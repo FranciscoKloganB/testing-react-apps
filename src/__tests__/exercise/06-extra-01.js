83dc98a24324cea74dcd8ede6040db2d364a17a0
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 13:42:45 +0000

    feat: mock useLocation hook instead of window API functions - closes #6.1

---
+++ src/__tests__/exercise/06-extra-01.js
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import {render, screen, act} from '@testing-library/react'
+import {useCurrentPosition} from 'react-use-geolocation'
+import Location from '../../examples/location'
+
+jest.mock('react-use-geolocation')
+
+describe('Location', () => {
+  let setLocation
+
+  const fakePosition = {
+    coords: {latitude: 33, longitude: 34},
+  }
+
+  beforeEach(() => {
+    function useMockCurrentPosition() {
+      const [state, setState] = React.useState([])
+      setLocation = setState
+      return state
+    }
+    useCurrentPosition.mockImplementation(useMockCurrentPosition)
+  })
+
+  it('displays the users current location', async () => {
+    render(<Location />)
+    
+    expect(useCurrentPosition).toHaveBeenCalledWith() // no args
+    expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+    act(() => {
+      setLocation([fakePosition])
+    })
+
+    expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/latitude/i)).toHaveTextContent(
+      `Latitude: ${fakePosition.coords.latitude}`,
+    )
+    expect(screen.getByText(/longitude/i)).toHaveTextContent(
+      `Longitude: ${fakePosition.coords.longitude}`,
+    )
+  })
+})
