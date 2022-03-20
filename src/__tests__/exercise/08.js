4e787aa801cd69cad9721fbd3e0d3a46be2dfe33
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 15:23:25 +0000

    feat: test useCount hook through component

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -11,12 +11,53 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
+function Counter(props) {
+  const {count, increment, decrement} = useCounter(props)
 
-test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  return (
+    <>
+      <p>count: {count}</p>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </>
+  )
+}
+
+describe('useCounter hook', () => {
+  describe('when used within a Counter component', () => {
+    it('exposes the count and increment/decrement functions', () => {
+      render(<Counter />)
+
+      const paragraph = screen.getByText(/count/i)
+      expect(paragraph).toHaveTextContent('0')
+
+      const incrementButton = screen.getByRole('button', {name: /increment/i})
+      userEvent.click(incrementButton)
+      expect(paragraph).toHaveTextContent('1')
+
+      const decrementButton = screen.getByRole('button', {name: /decrement/i})
+      userEvent.click(decrementButton)
+      expect(paragraph).toHaveTextContent('0')
+    })
+
+    it('accepts initial count prop', () => {
+      const initialCount = 3
+      render(<Counter initialCount={initialCount} />)
+
+      const paragraph = screen.getByText(/count/i)
+      expect(paragraph).toHaveTextContent(initialCount)
+    })
+
+    it('accepts step prop', () => {
+      const step = 4
+      render(<Counter step={step} />)
+
+      const paragraph = screen.getByText(/count/i)
+      const incrementButton = screen.getByRole('button', {name: /increment/i})
+      userEvent.click(incrementButton)
+      expect(paragraph).toHaveTextContent(step)
+    })
+  })
 })
 
 /* eslint no-unused-vars:0 */
