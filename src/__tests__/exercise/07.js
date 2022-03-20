ed32f70da487cd91d593adf7e9a0c6a029ead2c5
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 14:43:23 +0000

    refactor: simplify wrapper usability, closes #7.3

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,17 +2,17 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render as rtlRender, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
-import userEvent from '@testing-library/user-event'
 
-function Wrapper({children}) {
-  return <ThemeProvider initialTheme="light">{children}</ThemeProvider>
-}
+function renderWithTheme(ui, {theme = 'light', ...options} = {}) {
+  const wrapper = ({children}) => (
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  )
 
-function render(Component, options) {
-  return rtlRender(Component, {wrapper: Wrapper, ...options})
+  // Return the container for consumers of our API
+  return render(ui, { wrapper, ...options })
 }
 
 const defaultTheme = 'background-color: white; color: black;'
@@ -20,13 +20,13 @@ const darkTheme = 'background-color: black; color: white;'
 
 describe('EasyButton', () => {
   it('renders with the light styles for the light theme', () => {
-    render(<EasyButton>Easy</EasyButton>)
+    renderWithTheme(<EasyButton>Easy</EasyButton>)
     const button = screen.getByRole('button', {name: /easy/i})
     expect(button).toHaveStyle(defaultTheme)
   })
 
   it('renders with the dark styles for the light theme', () => {
-    render(<EasyButton initialTheme="dark">Easy</EasyButton>)
+    renderWithTheme(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
     const button = screen.getByRole('button', {name: /easy/i})
     expect(button).toHaveStyle(darkTheme)
   })
