3928d173edc38ce1c315b4484bec354a93bf006c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 15:36:55 +0000

    chore: rename files to represent  their actual contents

--- src/__tests__/exercise/08-extra-01.js
+++ src/__tests__/exercise/08-extra-01.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {act, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -11,52 +11,33 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
-function Counter(props) {
-  const {count, increment, decrement} = useCounter(props)
 
-  return (
-    <>
-      <p>count: {count}</p>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-    </>
-  )
-}
 
 describe('useCounter hook', () => {
-  describe('when used within a Counter component', () => {
-    it('exposes the count and increment/decrement functions', () => {
-      render(<Counter />)
+  let result
 
-      const paragraph = screen.getByText(/count/i)
-      expect(paragraph).toHaveTextContent('0')
+  /**
+   * When components are too complicated to test using example components like
+   * the one we had, we can instead follow the approach below. Where we only
+   * test the logic of the component but not the UI that results from using it.
+   */
+  function TestComponent(props) {
+    result = useCounter()
+    return null
+  }
 
-      const incrementButton = screen.getByRole('button', {name: /increment/i})
-      userEvent.click(incrementButton)
-      expect(paragraph).toHaveTextContent('1')
+  it('exposes the count and increment/decrement functions', () => {
+    render(<TestComponent />)
 
-      const decrementButton = screen.getByRole('button', {name: /decrement/i})
-      userEvent.click(decrementButton)
-      expect(paragraph).toHaveTextContent('0')
-    })
+    expect(result.count).toBe(0)
 
-    it('accepts initial count prop', () => {
-      const initialCount = 3
-      render(<Counter initialCount={initialCount} />)
+    // We need to use act, because react is not expecting state updates
+    // Again, by using act, we inform react that this state update was intended!
+    act(() => result.increment())
+    expect(result.count).toBe(1)
 
-      const paragraph = screen.getByText(/count/i)
-      expect(paragraph).toHaveTextContent(initialCount)
-    })
-
-    it('accepts step prop', () => {
-      const step = 4
-      render(<Counter step={step} />)
-
-      const paragraph = screen.getByText(/count/i)
-      const incrementButton = screen.getByRole('button', {name: /increment/i})
-      userEvent.click(incrementButton)
-      expect(paragraph).toHaveTextContent(step)
-    })
+    act(() => result.decrement())
+    expect(result.count).toBe(0)
   })
 })
 
